c7b49481c450f4a248d333c9295cbb07f61dc32e
Anand <anandsawwalakhe@gmail.com>
2017-10-27 15:42:55 +0530

    dsa

---
+++ js/docs-setup.js
@@ -0,0 +1,43 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "App",
+      "shortName": "App",
+      "type": "directive",
+      "moduleName": "App",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api app automatically content directive getcomputedstyle polyfill resize size text textarea window"
+    },
+    {
+      "section": "api",
+      "id": "Constant",
+      "shortName": "Constant",
+      "type": "service",
+      "moduleName": "Constant",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically constant content getcomputedstyle polyfill resize service size text textarea window"
+    },
+    {
+      "section": "api",
+      "id": "Directive",
+      "shortName": "Directive",
+      "type": "directive",
+      "moduleName": "Directive",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically content directive getcomputedstyle polyfill resize size text textarea window"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
